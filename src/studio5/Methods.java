072bc08467462738dc941eae1c11c54b766a9e2a
demariondelaney <demariondelaney@localhost>
2022-10-19 15:45:29 -0500

    HELLO STUDIO 5

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,5 +1,7 @@
 package studio5;
 
+import java.awt.Color;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
@@ -15,8 +17,11 @@ public class Methods {
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
+		distance = Math.sqrt(Math.pow((x2-x1),2)+(Math.pow((y2-y1), 2)));
+
+
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+
 		return distance;
 	}
 
@@ -29,24 +34,34 @@ public class Methods {
 	 */
 	public static void drawBullsEye(double x, double y, double radius) {
 		StdDraw.setPenColor(StdDraw.BLACK);
-		StdDraw.filledCircle(x, y, radius);
+		StdDraw.filledCircle(x, y, 1);
+
 
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
+		StdDraw.setPenColor(0,109,219);
+		StdDraw.filledCircle(x, y, 0.75);
+
 		// suggested rgb values: 0, 109, 219
 
-		
+
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x, y, 0.5);
 
-		
+
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x, y, 0.25);
 
-		
+
 	}
+
+
 
 	/**
 	 * Return a new String which is the original source String with all occurrences
@@ -61,8 +76,19 @@ public class Methods {
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
-		// TODO: Finish this method
+		char [] array = source.toCharArray();
 		
+		for (int i =0; i<source.length(); i++) {
+			if (array[i] == target) {
+				result = result + replacement;
+			}
+			else {
+				result = result + array[i];
+			}
+		}
+		System.out.println(result);
+		// TODO: Finish this method
+
 		return result;
 	}
 
@@ -75,7 +101,7 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+
 		return sum;
 	}
 
@@ -90,7 +116,7 @@ public class Methods {
 		int[] values = null; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
 
-		
+
 
 		return values;
 	}
@@ -98,5 +124,5 @@ public class Methods {
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
 
-	
+
 }
